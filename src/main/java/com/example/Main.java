920e8c18153209ffc366ef827f9e0fbcf64f9448
rylanturner02 <rylanmt2@illinois.edu>
2021-02-06 17:45:30 -0600

    Created SenateData.java and Senator.java.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,20 +1,18 @@
 package com.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-
 
 import java.io.File;
 import java.io.IOException;
-
-
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/json");
-        //There is no need for an adventure class, the example below is just for convenience
-        //if you're using jackson. It's also a preview to your next assignment :)
-        //Adventure adventure = new ObjectMapper().readValue(file, Adventure.class);
-        //Gson gson = new Gson();
+        Gson gson = new Gson();
+        Reader jsonReader = Files.newBufferedReader(Paths.get("senators.json"));
+        SenateData senateData = gson.fromJson(jsonReader, SenateData.class);
     }
 }
---
+++ src/main/java/com/example/SenateData.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.List;
+
+public class SenateData {
+    private List<Senator> senators;
+
+    public SenateData() {
+
+    }
+}
