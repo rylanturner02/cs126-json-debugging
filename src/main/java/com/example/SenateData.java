4202be8f158d68ee61adc4d466c16983e18c7b2b
rylanturner02 <rylanmt2@illinois.edu>
2021-02-06 17:54:43 -0600

    Added constructor and getSenators method to SenateData.

--- src/main/java/com/example/SenateData.java
+++ src/main/java/com/example/SenateData.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class SenateData {
     private List<Senator> senators;
 
-    public SenateData() {
+    public SenateData(List<Senator> newSenators) {
+        senators = newSenators;
+    }
 
+    public List<Senator> getSenators() {
+        return senators;
     }
 }
--- src/main/java/com/example/Senator.java
+++ src/main/java/com/example/Senator.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public class Senator {
+
+
     public Senator() {
 
     }
