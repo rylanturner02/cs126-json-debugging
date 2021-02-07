ed932ede8bb903730cf63d1605487983ec419215
rylanturner02 <rylanmt2@illinois.edu>
2021-02-06 18:04:51 -0600

    Added constructors and getters to Senator.java.

--- src/main/java/com/example/Senator.java
+++ src/main/java/com/example/Senator.java
@@ -1,9 +1,33 @@
 package com.example;
 
 public class Senator {
-
+    private String name;
+    private String gender;
+    private String birthday;
+    private String startdate;
+    private String state;
 
     public Senator() {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public String getState() {
+        return state;
     }
 }
