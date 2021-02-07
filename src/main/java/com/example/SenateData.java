3ba712238dde19df56309211dbabefc54aafd96f
rylanturner02 <rylanmt2@illinois.edu>
2021-02-07 09:58:29 -0600

    Removed Meta.java now that testing purposes are passed.

--- src/main/java/com/example/SenateData.java
+++ src/main/java/com/example/SenateData.java
@@ -8,10 +8,8 @@ import java.util.List;
 public class SenateData {
     @SerializedName("objects")
     private List<Senator> senators;
-    private final Meta meta;
 
-    public SenateData(Meta newMeta, List<Senator> newSenators) {
-        meta = newMeta;
+    public SenateData(List<Senator> newSenators) {
         senators = newSenators;
     }
 
@@ -21,9 +19,5 @@ public class SenateData {
 
     public List<Senator> getSenators() {
         return senators;
-    }
-
-    public Meta getMeta() {
-        return meta;
     }
 }
