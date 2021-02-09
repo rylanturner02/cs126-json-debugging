47425d333f8689a08054503d60360e514b62c5c3
rylanturner02 <rylanmt2@illinois.edu>
2021-02-09 02:00:10 -0600

    Added test names with general functionality for SenateFilteringTest.java.

---
+++ src/test/com/example/com/example/SenateFilteringTest.java
@@ -0,0 +1,133 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SenateFilteringTest {
+    private Gson gson;
+
+    @Before
+    public void setUp() {
+        gson = new Gson();
+    }
+
+    // ...
+
+    @Test
+    public void testFilteringForDemocratPass() {
+
+    }
+
+    @Test
+    public void testFilteringForRepublicanPass() {
+
+    }
+
+    @Test
+    public void testFilteringForIndependentPass() {
+
+    }
+
+    @Test
+    public void testFilteringForInvalidPartyPass() {
+
+    }
+
+    @Test
+    public void testFilteringForNonStringPartyPass() {
+
+    }
+
+    @Test
+    public void testFilteringForEmptyStringPartyPass() {
+
+    }
+
+    @Test
+    public void testFilteringForNullPartyPass() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testFilteringForValidBirthYearFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForTooHighBirthYearFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForInvalidBirthYearFloorPass() {
+
+    }
+
+    @Test
+    public void testFilteringForEmptyStringBirthYearFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForNullBirthYearFloor() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testFilteringForValidStartDateFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForTooHighStartDateFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForInvalidStartDateFloorPass() {
+
+    }
+
+    @Test
+    public void testFilteringForEmptyStringStartDateFloor() {
+
+    }
+
+    @Test
+    public void testFilteringForNullStartDateFloor() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testFilteringForValidStatePass() {
+
+    }
+
+    @Test
+    public void testFilteringForInvalidStatePass() {
+
+    }
+
+    @Test
+    public void testFilteringForNonStringStatePass() {
+
+    }
+
+    @Test
+    public void testFileringForEmptyStatePass() {
+
+    }
+
+    @Test
+    public void testFilteringForNullStatePass() {
+
+    }
+}
