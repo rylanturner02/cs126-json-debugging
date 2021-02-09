f6f88a67bcb1a125783f2c55a06883474182e9f7
rylanturner02 <rylanmt2@illinois.edu>
2021-02-09 13:25:29 -0600

    Added constructor with parameter in SenateAnalysis.

--- src/main/java/com/example/SenateAnalysis.java
+++ src/main/java/com/example/SenateAnalysis.java
@@ -1,4 +1,9 @@
 package com.example;
 
 public class SenateAnalysis {
+    private SenateData senateData;
+
+    public SenateAnalysis(SenateData newSenateData) {
+        senateData = newSenateData;
+    }
 }
--- src/test/com/example/com/example/SenateAnalysisTest.java
+++ src/test/com/example/com/example/SenateAnalysisTest.java
@@ -4,11 +4,24 @@ import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SenateAnalysisTest {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class SenateAnalysisTest {
+    private SenateAnalysis senateAnalysis;
+
+    /**
+     * Sets up senateData exactly like in Main.java and instantiates senateAnalysis.
+     * @throws IOException for Files.newBufferedReader().
+     */
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         Gson gson = new Gson();
+        Reader jsonReader = Files.newBufferedReader(Paths.get("src/main/resources/senators.json"));
+        SenateData senateData = gson.fromJson(jsonReader, SenateData.class);
+        senateAnalysis = new SenateAnalysis(senateData);
     }
 
     // ...
