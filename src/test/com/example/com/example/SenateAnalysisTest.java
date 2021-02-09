a842b24253dd7e361fa82e5444f19ee1920615b4
rylanturner02 <rylanmt2@illinois.edu>
2021-02-09 02:04:27 -0600

    Added test names with general functionality for SenateAnalysisTest.java.

--- src/test/com/example/com/example/SenateAnalysisTest.java
+++ src/test/com/example/com/example/SenateAnalysisTest.java
@@ -1,4 +1,47 @@
 package com.example;
 
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SenateAnalysisTest {
+    private Gson gson;
+
+    @Before
+    public void setUp() {
+        gson = new Gson();
+    }
+
+    // ...
+
+    @Test
+    public void testAnalysisForCorrectAverageStartYear() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testAnalysisForCorrectAverageBirthYear() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testAnalysisForCorrectSortedListByStartDate() {
+
+    }
+
+    // ...
+
+    @Test
+    public void testAnalysisForCorrectHighestFrequencyPartyGivenJuniorRank() {
+
+    }
+
+    @Test
+    public void testAnalysisForCorrectHighestFrequencyPartyGivenSeniorRank() {
+
+    }
 }
